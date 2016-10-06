68deb7f5a961f6f715287a64a835b1837e71ff14
Ivan Kozik <ivan@ludios.org>
2016-10-06 11:42:10 +0000

    List 'known' date as well

--- make_index.py
+++ make_index.py
@@ -15,7 +15,7 @@ print('''
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
-	<title>All predictions on predictionbook.com</title>
+	<title>All public predictions on predictionbook.com</title>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<style>
 		body {
@@ -35,7 +35,16 @@ print('''
 </head>
 <body>
 <table>
+<tr><td>#</td><td>predicted</td><td>known</td><td>user</td><td>prediction</td></tr>
 ''')
+
+def get_ymd_and_url(date):
+	ymd  = date.split()[0]
+	year, month, day = ymd.split('-')
+	day = day.lstrip('0')
+	month_name = calendar.month_name[int(month)]
+	date_url = 'https://en.wikipedia.org/wiki/Portal:Current_events/%s_%s_%s' % (year, month_name, day)
+	return ymd, date_url
 
 for n in nums:
 	with open("predictionbook/%s" % (n,)) as f:
@@ -49,16 +58,18 @@ for n in nums:
 		title = html.unescape(title)
 		prediction = title.replace("PredictionBook: ", "", 1)
 		user_url, username = re.findall('<a class="user" href="(.*?)">(.*?)</a>', content)[0]
-		date = re.findall('<span title="(.*?)" class="date">', content)[0]
-		ymd  = date.split()[0]
-		#print(n, user_url, username, day, prediction)
+		user_url = 'http://predictionbook.com' + user_url
+
 		url = 'http://predictionbook.com/predictions/%s' % (n,)
-		year, month, day = ymd.split('-')
-		day = day.lstrip('0')
-		month_name = calendar.month_name[int(month)]
-		date_url = 'https://en.wikipedia.org/wiki/Portal:Current_events/%s_%s_%s' % (year, month_name, day)
-		print('<tr><td><a href="%s">%s</a></td><td><a href="%s">%s</a></td><td><a href="%s">%s</a></td><td><a href="%s">%s</a></td></tr>' % (
-			url, n, date_url, ymd, user_url, username, url, html.escape(prediction)))
+
+		predicted_date = re.findall('<span title="(.*?)" class="date">', content)[0]
+		predicted_ymd, date_url = get_ymd_and_url(predicted_date)
+
+		known_date = re.findall('known <span title="(.*?)" class="date">', content)[0]
+		known_ymd, _ = get_ymd_and_url(known_date)
+
+		print('<tr><td><a href="%s">%s</a></td><td><a href="%s">%s</a></td><td>%s</td><td><a href="%s">%s</a></td><td><a href="%s">%s</a></td></tr>' % (
+			url, n, date_url, predicted_ymd, known_ymd, user_url, username, url, html.escape(prediction)))
 
 print('''
 </table>
