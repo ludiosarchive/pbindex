af3cd24690de3e4fcaad9be7db1294aa891d5f95
Ivan Kozik <ivan@ludios.org>
2017-10-26 14:10:01 +0000

    download.sh: update for https://

--- download.sh
+++ download.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-last_prediction=$(curl -s http://predictionbook.com/ | grep -o -P '/predictions/\d+' | sort -n | tail -n 1 | cut -d '/' -f 3)
+last_prediction=$(curl -sL https://predictionbook.com/ | grep -o -P '/predictions/\d+' | sort -n | tail -n 1 | cut -d '/' -f 3)
 mkdir -p predictionbook
 cd predictionbook
-for i in $(seq 1 1 $last_prediction); do
-    wget "http://predictionbook.com/predictions/$i" -O $i
+for i in $(seq 1 $last_prediction); do
+    wget "https://predictionbook.com/predictions/$i" -O $i
 done
